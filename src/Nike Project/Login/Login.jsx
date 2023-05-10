98d8914112c0bc01f6f6f1928717048ca4e0184f
Orangesmw <106233500+Orangesmw1@users.noreply.github.com>
2023-05-10 18:32:35 +0700

    done

--- src/Nike Project/Login/Login.jsx
+++ src/Nike Project/Login/Login.jsx
@@ -1,7 +1,53 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import logo from "../../assist/image/logo.png";
+
+import "./Login.css";
+import { NavLink } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <div className="login">
+      <div className="header-login">
+        <img src={logo} alt="" />
+        <h2>
+          YOUR ACCOUNT FOR <br /> EVERYTHING NIKE
+        </h2>
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("email")} placeholder="Email address" />
+        <input {...register("password")} placeholder="Password" />
+
+        <div className="check-box">
+          <div className="checkbox-login">
+            <input type="checkbox" />
+            <label>Keep me signed in</label>
+          </div>
+          <a href="">Forgotten your password?</a>
+        </div>
+
+        <div className="termp">
+          By logging in, you agree to Nike's
+          <a href="">Privacy Policy</a>
+          <br />
+          and
+          <a href="">Terms of Use</a>.
+        </div>
+
+        <input type="submit" value="Sign in" />
+
+        <div className="join-us">
+          Not a Member?
+          <h6>
+            <NavLink to="/register">Join Us</NavLink>
+          </h6>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- src/Nike Project/Register/Register.jsx
+++ src/Nike Project/Register/Register.jsx
@@ -1,64 +1,103 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import "./Register.css";
+import logo from "../../assist/image/logo.png";
+import { NavLink } from "react-router-dom";
 
 const Register = () => {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm();
+  const ref = useRef();
+
+  const { register, handleSubmit } = useForm();
 
   const onSubmit = (data) => {
-    alert(JSON.stringify(data));
+    console.log("data", data);
   };
 
   return (
     <div className="register">
       <form onSubmit={handleSubmit(onSubmit)}>
-        <label>Email address</label>
+        <div className="header-register">
+          <img src={logo} alt="" />
+          <h2>BECOME A NIKE MEMBER</h2>
+          <h6>
+            Create your Nike Member profile and get first access to the very
+            best of Nike products, inspiration and community.
+          </h6>
+        </div>
         <input
+          placeholder="Email address"
           type="email"
           {...register("emailAddress", {
             required: true,
           })}
         />
-        {errors?.emailAddress?.type === "required" && (
-          <p>This field is required</p>
-        )}
 
-        <label>First name</label>
         <input
+          placeholder="First name"
           {...register("firstName", {
             required: true,
             maxLength: 20,
             pattern: /^[A-Za-z]+$/i,
           })}
         />
-        {errors?.firstName?.type === "required" && (
-          <p>This field is required</p>
-        )}
-        {errors?.firstName?.type === "maxLength" && (
-          <p>First name cannot exceed 20 characters</p>
-        )}
-        {errors?.firstName?.type === "pattern" && (
-          <p>Alphabetical characters only</p>
-        )}
 
-        <label>Laste Name</label>
-        <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
-        {errors?.lastName?.type === "pattern" && (
-          <p>Alphabetical characters only</p>
-        )}
+        <input
+          placeholder="Last name"
+          {...register("lastName", {
+            pattern: /^[A-Za-z]+$/i,
+            required: true,
+          })}
+        />
 
-        <label>Age</label>
-        <input {...register("age", { min: 18, max: 99 })} />
-        {errors.age && (
-          <p>You Must be older then 18 and younger then 99 years old</p>
-        )}
-        <input type="submit" />
+        <input
+          {...register("dateBirth", {
+            required: true,
+          })}
+          placeholder="Date of Birth"
+          type="date"
+        />
+
+        <h6>Get a Nike Member Reward every year on your Birthday.</h6>
+
+        <select {...register("country")}>
+          <optgroup label="Country/Region">
+            <option value="hanoi">Ha Noi</option>
+            <option value="hcm">Ho Chi Minh</option>
+            <option value="danang">Da Nang</option>
+          </optgroup>
+        </select>
+
+        <ul className="gender" {...register("gender")}>
+          <input type="button" value="male" id="input-male" />
+
+          <input type="button" value="female" id="input-femate" />
+        </ul>
+
+        {/* <div className="check-box">
+          <input type="checkbox" />
+          <label>
+            Sign up for emails to get updates from Nike on products, offers and
+            your Member benefits
+          </label>
+        </div>
+
+        <div className="termp">
+          By creating an account, you agree to Nike's
+          <a href="">Privacy Policy</a>
+          <br />
+          and
+          <a href="">Terms of Use</a>.
+        </div> */}
+
+        <input type="submit" value="join us" />
       </form>
+
+      <div className="sign-in">
+        Already a Member?
+        <h6>
+          <NavLink to="/login">Sign In</NavLink>
+        </h6>
+      </div>
     </div>
   );
 };
